dbfa3e35fb0dc181e09204dd2f4f756da03a3a8b
Colton Gardner <Colton.Gardner@groupo.com>
2022-02-16 17:14:21 -0700

    lazyDays - adding counter for errorHandler id

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -4,17 +4,20 @@ import { QueryClient } from 'react-query';
 import { theme } from '../theme';
 
 const toast = createStandaloneToast({ theme });
+let counter = 0;
 
 function queryErrorHandler(error: unknown): void {
   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-  const id = 'react-query-error';
+  const id = `react-query-error-${counter}`;
+  counter += 1;
   const title =
     error instanceof Error ? error.message : 'error connecting to server';
 
-  console.log('in error handler');
+  console.log('in error handler id: ', id);
   // prevent duplicate toasts
   // had to comment out this line becuase its not letting the toast show on the Staff page
-  //   toast.closeAll();
+  // or adding counter to make the id unique, shows 1 error at a time.
+  toast.closeAll();
   toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
 }
 
