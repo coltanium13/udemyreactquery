eb3b525e60121531637c2231302ddf8ecd8a26c3
Colton Gardner <Colton.Gardner@groupo.com>
2022-02-17 07:53:27 -0700

    lazyDays - removing the id passed into the toast to fix error. different from lecture 36 where its left in

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -4,21 +4,18 @@ import { QueryClient } from 'react-query';
 import { theme } from '../theme';
 
 const toast = createStandaloneToast({ theme });
-let counter = 0;
 
 function queryErrorHandler(error: unknown): void {
   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-  const id = `react-query-error-${counter}`;
-  counter += 1;
   const title =
     error instanceof Error ? error.message : 'error connecting to server';
 
-  console.log('in error handler id: ', id);
+  console.log('in error handler');
   // prevent duplicate toasts
-  // had to comment out this line becuase its not letting the toast show on the Staff page
+  // removing the 'id' since this was breaking the toasts.
   // or adding counter to make the id unique, shows 1 error at a time.
   toast.closeAll();
-  toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
 }
 
 export const queryClient = new QueryClient({
