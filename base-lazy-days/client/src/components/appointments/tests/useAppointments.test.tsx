3ea5357d4fe2be49db181c59791778eb07396518
Colton Gardner <Colton.Gardner@groupo.com>
2022-02-28 16:09:55 -0700

    lazyDays - adding in working version of useAppointments custom hook test, and skipping lecture version, not working

--- base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -3,7 +3,32 @@ import { act, renderHook } from '@testing-library/react-hooks';
 import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
 
-test.skip('filter appointments by availability', async () => {
+test('filter appointments by availability', async () => {
+  const { result, waitFor } = renderHook(useAppointments, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for the appointments to populate
+  await waitFor(() => Object.keys(result.current.appointments).length > 0);
+  const filteredAppointmentsLength = result.current.appointments[1].length;
+
+  console.log(
+    '---- first result.current.appointments):',
+    result.current.appointments[1],
+  );
+  // set to show all appointments
+  act(() => result.current.setShowAll(true));
+
+  console.log('second time: ', result.current.appointments[1]);
+
+  // wait for the appointments to show more than when filtered
+  await waitFor(
+    () => result.current.appointments[1].length > filteredAppointmentsLength,
+  );
+});
+
+// from the lecture, but not working:
+test.skip('filter appointments by availability_NOT WORKING', async () => {
   const { result, waitFor } = renderHook(useAppointments, {
     wrapper: createQueryClientWrapper(),
   });
