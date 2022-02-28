fc0c210a3696abab3a2af5b142d7dd21cb28ed55
Colton Gardner <Colton.Gardner@groupo.com>
2022-02-28 12:30:18 -0700

    lazyDays - setup tests, and write some basic tests

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,16 @@
 import { screen } from '@testing-library/react';
 
+import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
   // write test here
+  renderWithQueryClient(<Treatments />);
+
+  // find all headings that have one of those three words case insensitive
+  const treatmentTitles = await screen.findAllByRole('heading', {
+    name: /massage|facial|scrub/i,
+  });
+
+  expect(treatmentTitles).toHaveLength(3);
 });
