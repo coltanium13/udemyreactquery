86b5f4c2b51ae3bbc10747407693d76d71182238
Colton Gardner <Colton.Gardner@groupo.com>
2022-02-28 13:14:51 -0700

    lazyDays - testing mutations

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -18,18 +18,22 @@ function queryErrorHandler(error: unknown): void {
   toast({ title, status: 'error', variant: 'subtle', isClosable: true });
 }
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      onError: queryErrorHandler,
-      staleTime: 600000, // 10 minutes
-      cacheTime: 900000, // 15 minutes (doesnt make sense for staleTime to exceed cacheTime)
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
+export function generateQueryClient(): QueryClient {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        onError: queryErrorHandler,
+        staleTime: 600000, // 10 minutes
+        cacheTime: 900000, // 15 minutes (doesnt make sense for staleTime to exceed cacheTime)
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+      },
+      mutations: {
+        onError: queryErrorHandler,
+      },
     },
-    mutations: {
-      onError: queryErrorHandler,
-    },
-  },
-});
+  });
+}
+
+export const queryClient = generateQueryClient();
--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,18 +1,31 @@
+/* eslint-disable no-console */
 import { render, RenderResult } from '@testing-library/react';
 import { ReactElement } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-// import { defaultQueryClientOptions } from '../react-query/queryClient';
+import { QueryClient, QueryClientProvider, setLogger } from 'react-query';
+
+import { generateQueryClient } from '../react-query/queryClient';
+
+setLogger({
+  log: console.log,
+  warn: console.warn,
+  error: () => {
+    // swallow errors without printing out
+  },
+});
 
 // make a function to generate a unique query client for each test
-const generateQueryClient = () => {
-  return new QueryClient();
+const generateTestQueryClient = () => {
+  const client = generateQueryClient();
+  const options = client.getDefaultOptions();
+  options.queries = { ...options.queries, retry: false };
+  return client;
 };
 
 export function renderWithQueryClient(
   ui: ReactElement,
   client?: QueryClient,
 ): RenderResult {
-  const queryClient = client ?? generateQueryClient();
+  const queryClient = client ?? generateTestQueryClient();
 
   return render(
     <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
